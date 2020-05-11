4eaba7f908a77f84c6d1e8e2deba14fc54afc2ef
Wanichun Sinpitak <kitaboons@gmail.com>
2020-05-11 09:43:00 +0700

    refactor

--- src/main/kotlin/user/UserController.kt
+++ src/main/kotlin/user/UserController.kt
@@ -1,14 +1,13 @@
 package user
 
-import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.*
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
 import user.model.UserRequest
 import user.model.UserResponse
 import user.repository.UserRepository
 
 @Controller
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 class UserController(private val userRepository: UserRepository) {
     @Post("/users")
     fun add(@Body userRequest: UserRequest): UserResponse {
